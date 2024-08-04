25eac35658709f9678b970590824f20bcc653f76
Muhammad Usman Yousuf <112505379+Usman-99@users.noreply.github.com>
2024-08-04 22:35:41 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer=(props)=>{
 return(
     <div className="row fixed-bottom">
-        <button className="btn btn-success col-3" onClick={()=>{
+        <button className="btn btn-success col-6" onClick={()=>{
             props.resetQuantity();
         }}>Reset</button>
         <div className="col-6 bg-dark text-center text-white">{`Total Amount : ${props.totalAmount}`}</div>
-        <button className="btn btn-primary col-3">Pay Now</button>
     </div>
 )
 };
